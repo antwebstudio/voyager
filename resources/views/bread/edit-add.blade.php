143dd643039fef047283d3340d9bbb9d99c6c07b
Hui Yang <chy1988@antwebstudio.com>
2020-05-17 16:38:23 +0800

    Allow setting of custom display label and add hint for field

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -55,40 +55,55 @@
                                 $dataTypeRows = $dataType->{($edit ? 'editRows' : 'addRows' )};
                             @endphp
 
-                            @foreach($dataTypeRows as $row)
-                                <!-- GET THE DISPLAY OPTIONS -->
-                                @php
-                                    $display_options = $row->details->display ?? NULL;
-                                    if ($dataTypeContent->{$row->field.'_'.($edit ? 'edit' : 'add')}) {
-                                        $dataTypeContent->{$row->field} = $dataTypeContent->{$row->field.'_'.($edit ? 'edit' : 'add')};
-                                    }
-                                @endphp
-                                @if (isset($row->details->legend) && isset($row->details->legend->text))
-                                    <legend class="text-{{ $row->details->legend->align ?? 'center' }}" style="background-color: {{ $row->details->legend->bgcolor ?? '#f0f0f0' }};padding: 5px;">{{ $row->details->legend->text }}</legend>
-                                @endif
+							@section('fields')
+								@foreach($dataTypeRows as $row)
+									<!-- GET THE DISPLAY OPTIONS -->
+									@php
+										$display_options = $row->details->display ?? NULL;
+										if ($dataTypeContent->{$row->field.'_'.($edit ? 'edit' : 'add')}) {
+											$dataTypeContent->{$row->field} = $dataTypeContent->{$row->field.'_'.($edit ? 'edit' : 'add')};
+										}
+									@endphp
+								
+									@if (isset($row->details->legend) && isset($row->details->legend->text))
+										<legend class="text-{{ $row->details->legend->align ?? 'center' }}" style="background-color: {{ $row->details->legend->bgcolor ?? '#f0f0f0' }};padding: 5px;">{{ $row->details->legend->text }}</legend>
+									@endif
 
-                                <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width ?? 12 }} {{ $errors->has($row->field) ? 'has-error' : '' }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
-                                    {{ $row->slugify }}
-                                    <label class="control-label" for="name">{{ $row->getTranslatedAttribute('display_name') }}</label>
-                                    @include('voyager::multilingual.input-hidden-bread-edit-add')
-                                    @if (isset($row->details->view))
-                                        @include($row->details->view, ['row' => $row, 'dataType' => $dataType, 'dataTypeContent' => $dataTypeContent, 'content' => $dataTypeContent->{$row->field}, 'action' => ($edit ? 'edit' : 'add'), 'view' => ($edit ? 'edit' : 'add'), 'options' => $row->details])
-                                    @elseif ($row->type == 'relationship')
-                                        @include('voyager::formfields.relationship', ['options' => $row->details])
-                                    @else
-                                        {!! app('voyager')->formField($row, $dataType, $dataTypeContent) !!}
-                                    @endif
+									<div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width ?? 12 }} {{ $errors->has($row->field) ? 'has-error' : '' }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
+										{{ $row->slugify }}
+										
+										@if (!isset($row->details->displayLabel) || $row->details->displayLabel !== false)
+											<label class="control-label {{ $row->required || strpos($row->details->validation->rule ?? '', 'required') !== false  ? 'required' : '' }}" for="name">
+												{{ $row->details->displayLabel ?? $row->getTranslatedAttribute('display_name') }}
+											</label>
+										@endif
+										
+										@include('voyager::multilingual.input-hidden-bread-edit-add')
+										@if (isset($row->details->view))
+											@include($row->details->view, ['row' => $row, 'dataType' => $dataType, 'dataTypeContent' => $dataTypeContent, 'content' => $dataTypeContent->{$row->field}, 'action' => ($edit ? 'edit' : 'add'), 'view' => ($edit ? 'edit' : 'add'), 'options' => $row->details])
+										@elseif ($row->type == 'relationship')
+											@include('voyager::formfields.relationship', ['options' => $row->details])
+										@else
+											{!! app('voyager')->formField($row, $dataType, $dataTypeContent) !!}
+										@endif
+										
+										@if (isset($row->details->hint))
+											<p>{{ $row->details->hint }}</p>
+										@endif
 
-                                    @foreach (app('voyager')->afterFormFields($row, $dataType, $dataTypeContent) as $after)
-                                        {!! $after->handle($row, $dataType, $dataTypeContent) !!}
-                                    @endforeach
-                                    @if ($errors->has($row->field))
-                                        @foreach ($errors->get($row->field) as $error)
-                                            <span class="help-block">{{ $error }}</span>
-                                        @endforeach
-                                    @endif
-                                </div>
-                            @endforeach
+										@foreach (app('voyager')->afterFormFields($row, $dataType, $dataTypeContent) as $after)
+											{!! $after->handle($row, $dataType, $dataTypeContent) !!}
+										@endforeach
+										@if ($errors->has($row->field))
+											@foreach ($errors->get($row->field) as $error)
+												<span class="help-block">{{ $error }}</span>
+											@endforeach
+										@endif
+									</div>
+								@endforeach
+							
+							@endsection
+							@yield('fields')
 
                         </div><!-- panel-body -->
 
